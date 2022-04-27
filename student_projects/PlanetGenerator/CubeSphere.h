cc622c92a2b3e46c9d5ef2dc0f27585f9f59665b
patrick2102 <48348311+patrick2102@users.noreply.github.com>
2022-04-27 13:45:25 +0200

    Might make a cube dunno haven't tested yet

--- student_projects/PlanetGenerator/CubeSphere.h
+++ student_projects/PlanetGenerator/CubeSphere.h
@@ -2,6 +2,7 @@
 // Created by Patrick on 27/04/2022.
 //
 
+#include <utility>
 #include <vector>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
@@ -11,23 +12,32 @@
 
 #endif //ITU_GRAPHICS_PROGRAMMING_CUBESPHERE_H
 
+struct Vertex
+{
+    // Position
+    glm::vec3 Position;
+};
+
 class Side
 {
 public:
     unsigned int VBO;
+    glm::vec3 normal;
     std::vector<glm::vec3> vboVec;
 
-    Side(std::vector<glm::vec3> vboVec)
+
+    Side(std::vector<glm::vec3> vboVec, glm::vec3 normal)
     {
-        vboVec = vboVec;
+        this->normal = normal;
+        this->vboVec = std::move(vboVec);
         Setup();
     }
 
     void Setup()
     {
-        //glGenBuffers(1, &VBO);
-        //glBindBuffer(GL_ARRAY_BUFFER, VBO);
-        //glBufferData(GL_ARRAY_BUFFER, vboVec.size() * sizeof(Vertex), &vboVec[0], GL_STATIC_DRAW);
+        glGenBuffers(1, &VBO);
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, vboVec.size() * sizeof(Vertex), &vboVec[0], GL_STATIC_DRAW);
 
     }
 
@@ -42,14 +52,16 @@ class CubeSphere
 public:
     float pi = glm::pi<float>();
     glm::vec3 position;
-    float radius;
+    glm::vec3 normal;
     std::vector<Side> sides;
+    float radius;
 
     CubeSphere(glm::vec3 position, float radius)
     {
-        position = position;
-        radius = radius;
-        //sides = CreateCubeSphere(0);
+        this->position = position;
+        this->radius = radius;
+        normal = glm::vec3(0.0f, 0.0f, -0.0f);
+        CreateCube(radius);
     }
 
     void Draw(Shader* shader)
@@ -66,66 +78,62 @@ public:
 
 private:
 
-    std::vector<glm::vec3> CreateCube(float width)
+    void CreateCube(float width)
     {
-        //TODO transformation in shader
-        std::vector<float> vboVec;
+        sides.clear();
 
-        std::vector<glm::vec3> rightFace = FaceOfSquare(width);
-        /*
-        std::vector<glm::vec3> leftFace = FaceOfSquare(width);
-        std::vector<glm::vec3> topFace = FaceOfSquare(width);
-        std::vector<glm::vec3> bottomFace = FaceOfSquare(width);
-        std::vector<glm::vec3> frontFace = FaceOfSquare(width);
-        std::vector<glm::vec3> backFace = FaceOfSquare(width);
-        */
-
-        glm::mat4 model = glm::mat4(1.0f);
-
-        float radiansToRotate = pi/2;
-        glm::vec3 front = glm::vec3(0.0f, 1.0f, 0.0f);
-
-        auto rotation = glm::rotate(model, radiansToRotate, glm::vec3(0.0f, 0.0f, 0.0f));
-
-        /*
-        for(int i = 0; i < 6; i++)
+        float rotation = 0.0f;
+        glm::vec3 rotationAxis = glm::vec3(0.0f, 1.0f, 0.0f);
+        for(int i = 0; i < 4; i++)
         {
-            auto res = rotation * rightFace[i];
+            Side side = SideOfSquare(width, rotation, rotationAxis);
+            sides.insert(sides.end(), side);
+            rotation += pi/2;
         }
-        */
-        return rightFace;
+
+        rotation = pi/2;
+        rotationAxis = glm::vec3(1.0f, 0.0f, 0.0f);
+
+        for(int i = 0; i < 2; i++)
+        {
+            Side side = SideOfSquare(width, rotation, rotationAxis);
+            sides.insert(sides.end(), side);
+            rotation += pi;
+        }
     }
 
-    std::vector<glm::vec3> FaceOfSquare(float width)
+    Side SideOfSquare(float width, float rotation, glm::vec3 rotationAxis)
     {
+        // The 4 different corners of the square
+        float p0x = -width/2, p0y = -width/2, p0z = 0; // p0x, p0y are the lowest x and y values, p0z is 0 before transformation.
+        float p1x = width/2, p1y = width/2; // p1x and p1y are the highest x and y values.
+
+        glm::mat4 model = glm::mat4(1.0f);
+        model = glm::rotate(model, rotation, rotationAxis);
+
+        glm::vec3 sideNormal = model * glm::vec4(normal,0.0f);
+        model = glm::translate(model, sideNormal);
+
+        glm::vec3 topLeft = model * glm::vec4 (p0x, p1y, p0z, 1.0f);
+        glm::vec3 topRight = model * glm::vec4 (p1x, p1y, p0z, 1.0f);
+        glm::vec3 bottomLeft = model * glm::vec4 (p0x, p0y, p0z, 1.0f);
+        glm::vec3 bottomRight = model * glm::vec4 (p1x, p0y, p0z, 1.0f);
 
         std::vector<glm::vec3> coords;
 
-        // The 4 different corners of the square
-        float p0x = -width/2, p0y = -width/2, p0z = width/2; // p0x, p0y are the lowest x and y values, p0z is 0 before transformation.
-        float p1x = width/2, p1y = width/2; // p1x and p1y are the highest x and y values.
-
-
         // Top left triangle
-        coords.insert(coords.end(), glm::vec3(p0x, p1y, p0z)); // Top left corner
-        coords.insert(coords.end(), glm::vec3(p1x, p1y, p0z)); // Top right corner
-        coords.insert(coords.end(), glm::vec3(p0x, p0y, p0z)); // Bottom left corner
-
+        coords.insert(coords.end(), topLeft); // Top left corner
+        coords.insert(coords.end(), topRight); // Top right corner
+        coords.insert(coords.end(), bottomLeft); // Bottom left corner
 
         //Bottom right triangle
-        coords.insert(coords.end(), glm::vec3(p1x, p0y, p0z)); // Bottom right corner
-        coords.insert(coords.end(), glm::vec3(p0x, p0y, p0z)); // Bottom left corner
-        coords.insert(coords.end(), glm::vec3(p1x, p1y, p0z)); // Top right corner
+        coords.insert(coords.end(), bottomRight); // Bottom right corner
+        coords.insert(coords.end(), bottomLeft); // Bottom left corner
+        coords.insert(coords.end(), topRight); // Top right corner
 
-        //Side side = Side()
+        Side side = Side(coords, sideNormal);
 
-        return coords;
+        return side;
     }
 
-    std::vector<glm::vec3> CreateCubeSphere(int divisionLevels)
-    {
-        //glGenVertexArrays(1, &VBO);
-        std::vector<glm::vec3> cube = CreateCube(radius);
-        return cube;
-    }
 };
